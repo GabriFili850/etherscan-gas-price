f321cd89f44313ef97d510e0fd18de6157c10123
GabriFili850 <gabriele.filigheddu@live.it>
2023-04-09 21:53:18 +0100

    moves file location

---
+++ src/GasPrice.js
@@ -0,0 +1,56 @@
+// src/GasPrice.js
+
+import React, { useState, useEffect } from "react";
+import axiosInstance from "./axiosInstance"; // Import the custom axios instance
+import { GasPriceContainer } from "./styles";
+
+const GasPrice = () => {
+  const [gasPrice, setGasPrice] = useState(null);
+  const [countdown, setCountdown] = useState(10);
+  const apiKey = process.env.REACT_APP_ETHERSCAN_API_KEY;
+
+  useEffect(() => {
+    const fetchGasPrice = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${apiKey}`
+        );
+        const gasPriceInGwei = parseInt(response.data.result, 16) / 1e9;
+        setGasPrice(gasPriceInGwei.toFixed(2));
+      } catch (error) {
+        console.error("Error fetching gas price:", error);
+      }
+    };
+
+    const updateCountdown = () => {
+      setCountdown((prevCountdown) => {
+        if (prevCountdown === 1) {
+          fetchGasPrice();
+          return 10;
+        } else {
+          return prevCountdown - 1;
+        }
+      });
+    };
+
+    fetchGasPrice(); // Initial fetch
+    const interval = setInterval(updateCountdown, 1000); // Update countdown every second
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <GasPriceContainer>
+      {gasPrice ? (
+        <>
+          <p>Current Gas Price: {gasPrice} Gwei</p>
+          <p>Refreshing in {countdown} seconds...</p>
+        </>
+      ) : (
+        <p>Loading gas price...</p>
+      )}
+    </GasPriceContainer>
+  );
+};
+
+export default GasPrice;
