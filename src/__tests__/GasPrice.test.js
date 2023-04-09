e59087589f1e47100392a4d7c94b9def8d122553
GabriFili850 <gabriele.filigheddu@live.it>
2023-04-09 22:46:00 +0100

    test passes without warning

--- src/__tests__/GasPrice.test.js
+++ src/__tests__/GasPrice.test.js
@@ -1,15 +1,28 @@
+// src/GasPrice.test.js
+
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import axios from "axios";
 import GasPrice from "../GasPrice";
 
-// Add this line to mock the axios module
 jest.mock("axios");
 
 describe("GasPrice component", () => {
-  test("renders loading message", () => {
-    render(<GasPrice />);
-    const loadingElement = screen.getByText(/Loading gas price.../i);
-    expect(loadingElement).toBeInTheDocument();
+  test("renders gas price", async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        result: "0x" + (20 * 1e9).toString(16), // Mock the response data to return 20 Gwei
+      },
+    });
+
+    // Wrap the render call with act and make the callback async
+    await act(async () => {
+      render(<GasPrice />);
+    });
+
+    const gasPriceElement = screen.getByText(/Current Gas Price: 20.00 Gwei/i);
+    expect(gasPriceElement).toBeInTheDocument();
+
+    // Add more tests here
   });
 });
